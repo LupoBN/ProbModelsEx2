2f94a736a6013bbed5c8da4634a50cc76f4fba7e
matan <matanbn22@gmail.com>
2018-01-03 20:02:32 +0200

    Hopefully working EM (but probably dosen't ): )

--- EM.py
+++ EM.py
@@ -9,8 +9,8 @@ class EM(object):
         self._P = list()
         self._one_divided_by_N = 1.0 / float(len(articles))
         self._ntk = articles
+        self._article_clusters = article_clusters
         self._initialize_parameters(num_of_topics, articles, article_clusters)
-
 
     def _initialize_parameters(self, num_of_topics, articles, article_clusters):
         for i in range(0, num_of_topics):
@@ -32,40 +32,40 @@ class EM(object):
             for word in cluster:
                 cluster[word] /= total_cluster_words
 
-    def _calculate_wti_numerator(self, i):
+    def _calculate_wti_numerator(self, i, t):
         # TODO: Add the smoothing.
         wti = self._alphas[i]
         for word in self._P[i]:
-            wti *= math.pow(self._P[i][word], self._ntk[word])
+            wti *= math.pow(self._P[i][word], self._ntk[t][word])
         return wti
 
-
-    def _update_alphas(self, wt):
+    def _update_alphas(self, w):
         for i in range(0, len(self._alphas)):
-            self._alphas[i] = self._one_divided_by_N * wt[i]
+            self._alphas[i] = self._one_divided_by_N
+            for t in range(0, len(w)):
+                self._alphas[i] *= w[t][i]
 
-    def _update_P(self, wt):
-        for cluster in self._P:
-            for word in cluster:
-                raise NotImplementedError
+    def _update_P(self, w):
+        for i in range(0,len(self._P)):
+            for word in self._P[i]:
+                numerator = 0.0
+                denominator = 0.0
+                for t in range(0, len(w)):
+                    numerator += w[t][i] * self._ntk[t][word]
+                    denominator += w[t][i] * sum(self._ntk[t].values())
+
+                self._P[i][word] = numerator / denominator
 
     def update_parameters(self):
-        wt = list()
-        for i in range(0, len(self._alphas)):
-            wti = self._calculate_wti_numerator(i)
-            wt.append(wti)
-        wti_sums = sum(wt)
-        for i in range(0, len(wt)):
-            wt[i] /= wti_sums
-        self._update_alphas(wt)
-        self._update_P(wt)
-
-
-
-
-
-
-
-        
-
-
+        w = list()
+        for t in range(0, len(self._ntk)):
+            w.append(list())
+            for i in range(0, len(self._alphas)):
+                wti = self._calculate_wti_numerator(i, t)
+                w[t].append(wti)
+        for t in range(0, len(w)):
+            alpha_j_sum = sum(w[i])
+            for i in range(0, len(self._alphas)):
+                w[t][i] /= alpha_j_sum
+        self._update_alphas(w)
+        self._update_P(w)
